062699ff116a254842ab59e033920e3f4bda467c
Maryia Velesiuk <m.velesiuk@andersenlab.com>
2024-06-23 16:50:46 +0300

    task4: created getPtoductsList function

--- products-service/lambda/getProductsList.mjs
+++ products-service/lambda/getProductsList.mjs
@@ -1,15 +1,48 @@
-import {products} from './productsMockData.mjs'
+import { DynamoDBClient, ScanCommand } from '@aws-sdk/client-dynamodb';
+import { unmarshall } from "@aws-sdk/util-dynamodb";
 
-export const handler = async (event) => {
+export const handler = async () => {
 
+  const productParams = {
+    TableName: "products"
+  };
+  const stockParams = {
+    TableName: "stocks"
+  };
+
+  try {
+    const client = new DynamoDBClient();
+    
+    const products = (await client.send(new ScanCommand(productParams))).Items.map((Item) => unmarshall(Item));
+    const stocks = (await client.send(new ScanCommand(stockParams))).Items.map((Item) => unmarshall(Item)); 
+
+    const result = products.map((product)=>({
+      id: product.id,
+      count: stocks.find(stock=>stock.product_id===product.id).count,
+      title: product.title,
+      description: product.description,
+      price: product.price
+    }))
+
+    return sendResponse(200, result);
+  } catch (err) {
+    const error = err;
+    return sendResponse(500, error.message);
+  }
+};
+
+export const sendResponse = (
+  statusCode=200,
+  body
+) => {
   return {
-    statusCode: 200,
-    headers: { 
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET",
-      "Access-Control-Allow-Headers": "Content-Type",
-      "Content-Type": "application/json",
+    statusCode,
+    body: JSON.stringify(body || {}),
+    headers: {
+      'Access-Control-Allow-Methods': '*',
+      'Access-Control-Allow-Headers': '*',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
     },
-    body: JSON.stringify(products),
   };
 };
--- products-service/lib/products-service-stack.ts
+++ products-service/lib/products-service-stack.ts
@@ -3,27 +3,45 @@ import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Role, ServicePrincipal, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 export class ProductsServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const role = new Role(this, "dynamodbAccessRole", {
+      assumedBy: new ServicePrincipal("lambda.amazonaws.com"),
+    });
+
+    role.addToPolicy(
+      new PolicyStatement({
+        actions: ["dynamodb:*", "logs:PutLogEvents"],
+        resources: ["*"],
+      })
+      );
+
     const productsFunction = new lambda.Function(this, 'ProductsFunction', {
-      runtime: lambda.Runtime.NODEJS_20_X, // Choose any supported Node.js runtime
+      runtime: lambda.Runtime.NODEJS_20_X,
       environment: {
         PRODUCT_AWS_REGION: 'eu-west-1',
+        PRODUCTS_TABLE: 'products',
+        STOCKS_TABLE: 'stocks',
       },
       code: lambda.Code.fromAsset('lambda'),
       handler: 'getProductsList.handler',
+      role
     });
 
     const productByIdFunction = new lambda.Function(this, 'ProductByIdFunction', {
       runtime: lambda.Runtime.NODEJS_20_X,
       environment: {
         PRODUCT_AWS_REGION: 'eu-west-1',
+        PRODUCTS_TABLE: "products",
+        STOCKS_TABLE: 'stocks',
       },
       code: lambda.Code.fromAsset('lambda'),
       handler: 'getProductsById.handler',
+      role
     });
 
     const api = new apigateway.RestApi(this, 'ProductApi', {
